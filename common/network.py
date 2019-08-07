81489cb36e7c3cf2749891f620bba3d0dcc46c5f
Jonas Melin <jonas.melin.x@gmail.com>
2019-08-07 20:25:25 +0200

    parameterSearch: larger kernels win

--- common/network.py
+++ common/network.py
@@ -18,11 +18,11 @@ def defineFCModel(networkInputLen, networkOutputLen, per_process_gpu_memory_frac
 
         # CNN feature extraction layers
         layer = tf.reshape(retValxFC, [-1, int(retValxFC.shape[1]), 1])
-        layer = tf.layers.conv1d(layer, 32, 32, 2, padding='same', activation=tf.nn.leaky_relu)
-        layer = tf.layers.conv1d(layer, 24, 24, 2, padding='same', activation=tf.nn.leaky_relu)
-        layer = tf.layers.conv1d(layer, 18, 16, 2, padding='same', activation=tf.nn.leaky_relu)
-        layer = tf.layers.conv1d(layer, 12, 12, 2, padding='same', activation=tf.nn.leaky_relu)
-        layer = tf.layers.conv1d(layer, 9, 10, 1, padding='same', activation=tf.nn.leaky_relu)
+        layer = tf.layers.conv1d(layer, 32, 48, 2, padding='same', activation=tf.nn.leaky_relu)
+        layer = tf.layers.conv1d(layer, 24, 32, 2, padding='same', activation=tf.nn.leaky_relu)
+        layer = tf.layers.conv1d(layer, 18, 24, 2, padding='same', activation=tf.nn.leaky_relu)
+        layer = tf.layers.conv1d(layer, 12, 18, 2, padding='same', activation=tf.nn.leaky_relu)
+        layer = tf.layers.conv1d(layer, 9, 14, 1, padding='same', activation=tf.nn.leaky_relu)
 
         layer = tf.reshape(layer, [-1, int(layer.shape[1] * layer.shape[2])])
 
